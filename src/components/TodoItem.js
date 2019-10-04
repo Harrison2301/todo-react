3ae5c7c4132177a0c63d3e54a76588181225e290
Harrison2301 <harrisonwinters2301@gmail.com>
2019-10-04 15:47:36 +0000

    added checkbox for completed todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,20 +4,22 @@ import PropTypes from 'prop-types';
 class TodoItem extends Component {
 
 getStyle = () => {
-  if(this.props.todo.completed) {
-    return {
-      textDecoration: 'line-through'
-    } 
-    } else {
-      return {
-        textDecoration: 'none'
-      }
+  return {
+    background: '#f4f4f4',
+    padding: '10px',
+    borderBottom: '1px #ccc dotted',
+    textDecoration: this.props.todo.completed ? 'line-through' : 'none'
   }
 }
+
   render() {
+    const { id, title} = this.props.todo;
     return (
         <div style={this.getStyle()}>
-        <p>{ this.props.todo.title }</p>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind (this, id)}/> {' '}
+        { title }
+        </p>
         </div>
     )
   }
