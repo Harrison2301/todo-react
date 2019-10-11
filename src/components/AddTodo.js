497ca9c031572f424759c24660088d29714327b2
Harrison2301 <harrisonwinters2301@gmail.com>
2019-10-11 15:34:14 +0000

    added input title to state

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
 
 export class AddTodo extends Component {
+    state = {
+        title: ''
+    }
+
+    onChange = (e) => this.setState({title: e.target.value});
+
     render(){
         return (
             <form style={{ display: "flex"}}>
-                <input type="text" name="title" placeholder="Add Todo" style={{flex: '10', padding: '5px' }} />
+                <input type="text" name="title" placeholder="Add Todo" style={{flex: '10', padding: '5px' }} 
+                value = {this.state.title}
+                onChange={this.onChange}
+                />
                 <input type="submit" value="Submit" className="btn" style={{flex: "1"}}/>
 
               </form>  
