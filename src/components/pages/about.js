186be198cfa0ec878c9c299291e3222b81d41d5b
Harrison2301 <harrisonwinters2301@gmail.com>
2019-10-15 00:28:26 +0000

    updated about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -4,7 +4,7 @@ function About() {
     return(
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the TodoList</p>
+            <p>This is a simple to do list app! I built this using for a quick refresh on React.JS</p>
         </React.Fragment>
     )
 }
